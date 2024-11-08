a3ffa10d3433878e55c33b9a53fd659bd0d403b5
oliverhuangcode <oliverwhuang@gmail.com>
2024-11-09 02:47:29 +1100

    Fix error with overlaps

--- components/rush-hour/levels.tsx
+++ components/rush-hour/levels.tsx
@@ -30,7 +30,7 @@ export const levels: CarType[][] = [
   ],
   [
     // Level 3 configuration
-    { id: 1, x: 0, y: 2, length: 2, orientation: "horizontal", isMain: true, color: "red-500", image: "/rush-hour/pinkcar-h.png" },
+    { id: 1, x: 1, y: 2, length: 2, orientation: "horizontal", isMain: true, color: "red-500", image: "/rush-hour/pinkcar-h.png" },
     { id: 2, x: 3, y: 0, length: 3, orientation: "horizontal", isMain: false, color: "yellow-500", image: "/rush-hour/longcar-h.png" },
     { id: 3, x: 3, y: 1, length: 3, orientation: "vertical", isMain: false, color: "yellow-500", image: "/rush-hour/longcar-v.png" },
     { id: 4, x: 2, y: 0, length: 2, orientation: "vertical", isMain: false, color: "pink-500", image: "/rush-hour/policecar-v.png" },
